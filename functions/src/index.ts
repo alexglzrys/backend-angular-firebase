a8a66e35e8735b561e6f9ccdcc0f3a902399cdec
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-26 13:17:27 -0600

    obtener colecciones

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require("serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+// Referencia a la base de datos para este proyecto en Firebase
+const db = admin.firestore();
 
 
 // // Start writing Firebase Functions
@@ -17,3 +20,17 @@ export const helloWorld = functions.https.onRequest((request, response) => {
       'command': 'firebase serve'
   });
 });
+
+export const getGOTY = functions.https.onRequest(async(req, res) => {
+  // Obtener el valor de un Query String
+  const name = req.query.name || 'Sin nombre';
+
+  // Obtener referencia a la base de datos Firestore
+  const gotyRef = db.collection('goty');
+  // Consultar el estado actual de mi colecciòn
+  const docsSnap = await gotyRef.get();
+  // Recuparar la informaciòn de todos los documentos registrados en la colecciòn
+  const juegos = docsSnap.docs.map(juego => juego.data());
+
+  res.json(juegos)
+})
